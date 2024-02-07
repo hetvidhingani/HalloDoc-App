ff94ad016101a5640b88462d3fe46454d955109f
hetvidhingani <hetvi.dhingani@etatvasoft.com>
2024-02-07 09:30:47 +0530

    added role table

---
+++ HalloDoc/DataModels/Role.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace HalloDoc.DataModels;
+
+[Table("Role")]
+[Index("RoleName", Name = "Unique_RoleName", IsUnique = true)]
+public partial class Role
+{
+    [Key]
+    [Column("RoleID")]
+    public long RoleId { get; set; }
+
+    [StringLength(50)]
+    public string RoleName { get; set; } = null!;
+}
